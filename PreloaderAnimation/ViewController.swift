89315fd9c6ece63f488d1f52e0eab3928fccc650
Anton Larchenko <anton2030t@gmail.com>
2020-06-24 13:17:07 +0300

    Initial Commit

---
+++ PreloaderAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PreloaderAnimation
+//
+//  Created by Anton Larchenko on 24.06.2020.
+//  Copyright © 2020 Anton Larchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
